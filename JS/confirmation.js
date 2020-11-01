5a4347138c642a4ad5a2c31a505a7d2ab32c3a99
Victor Combettes <vivi_c@hotmail.fr>
2020-11-01 18:17:40 +0100

    Création fonction POST

--- JS/confirmation.js
+++
@@ -1,8 +0,0 @@
-const titre = document.getElementById('test');
-
-function okTest () {
-        let okok = localStorage.getItem('testeu');
-        titre.textContent = okok;
-};
-
-okTest ();
--- JS/panier.js
+++ JS/panier.js
@@ -1,12 +1,14 @@
 let totalPrice = document.getElementById("totalPrice");
 let deletPanier = document.getElementById("deletPanier");
-let sendContact = document.getElementById("sendContact");
-let formContact = document.getElementsByTagName("form");
+let submit = document.querySelector("#form input[type='submit']");
+let formContact = document.getElementById("form");
+
+let resultPanier = JSON.parse(localStorage.getItem('countTable'));
+
 
 /* Création fonction permettant l'importation des données du localStorage et la création d'un tableau contenant ces données */
 function createTableProduit () {
 
-    let resultPanier = JSON.parse(localStorage.getItem('countTable'));
     let total = 0;
 
     for (let i = 0; i < (resultPanier.length)/4 ; i++) {
@@ -19,7 +21,6 @@ function createTableProduit () {
 
     /* Transformation du DOM au niveau de l'élément "total" selon le prix de chaque produit choisi */
     for (let i = 0; i < (resultPanier.length)/4 ; i++) {
-
         let priceNumber = parseInt(resultPanier[3 + i *4]);
         total += priceNumber;
         };
@@ -28,7 +29,6 @@ function createTableProduit () {
 
     /* Création fonction permmettant de nettoyer le localStorage */
     deletPanier.addEventListener('click', function (e) {
-
             localStorage.clear();
         });
 };
@@ -37,14 +37,66 @@ createTableProduit ();
 
 
 
-function sendContactProduit () {
+Send = (Request) => {
 
-    const Teste = "TESTE";
-
-    sendContact.addEventListener ('click', function(e) {
-
-            localStorage.setItem('testeu', Teste);
-    });
+  return new Promise((resolve) => {
+    let request = new XMLHttpRequest();
+    let testSend = document.getElementById("testSend");
+    request.onreadystatechange = function () {
+      if (this.readyState == XMLHttpRequest.DONE && this.status == 201) {
+        sessionStorage.setItem("order", this.responseText);
+        resolve(JSON.parse(this.responseText));
+        testSend.innerHTML = "test ok";
+      }
+    };
+    request.open("POST", "http://localhost:3000/api/teddies/order");
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(JSON.stringify(Request));
+  })
 };
 
-sendContactProduit ();
+
+
+function objetRequest () {
+submit.addEventListener('click', function(e) {
+
+  e.preventDefault();
+
+  const contact = {
+    prénom : document.getElementById("prénom").value,
+    nom : document.getElementById("nom").value,
+    adresse : document.getElementById("adresse").value,
+    ville : document.getElementById("ville").value,
+    email : document.getElementById("email").value,
+  };
+
+  const products = [];
+
+  for (let i = 0; i < (resultPanier.length)/4 ; i++) {
+    if (resultPanier[1 + i * 4] == "Norbert") {
+      products.push("5be9c8541c9d440000665243")
+
+    } else if (resultPanier[1 + i * 4] == "Arnold") {
+      products.push("5beaa8bf1c9d440000a57d94")
+
+    } else if (resultPanier[1 + i * 4] == "Lenny and Carl") {
+      products.push("5beaaa8f1c9d440000a57d95")
+        
+    } else if (resultPanier[1 + i * 4] == "Gustav") {
+      products.push("5beaabe91c9d440000a57d96")
+
+    } else if (resultPanier[1 + i * 4] == "Garfunkel") {
+      products.push("5beaacd41c9d440000a57d97")
+    }
+  };
+
+  let Request  = {
+    contact,
+    products
+  };
+
+  Send (Request);
+})
+}
+
+objetRequest ()
